f6abc1e53e49910c702b8f3d3abd8e04781f4158
Yu-Ji19 <ji.yu.vy@gmail.com>
2019-07-18 15:16:21 +0800

    Initial commit

---
+++ src/component/forms/LoginForm.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Form, Button, Label } from "semantic-ui-react";
+import Validator from 'validator';
+import InlineError from "../messages/InlineError";
+
+class LoginForm extends React.Component {
+  state = {
+    data: {
+      email: "",
+      password: ""
+    },
+    loading: false,
+    errors: {}
+  };
+
+  onChange = e =>
+    this.setState({
+      data: { ...this.state.data, [e.target.name]: e.target.value }
+    });
+
+  onSubmit = () => {
+    const errors = this.validate(this.state.data);
+    this.setState({errors});
+  };
+
+  validate = (data) => {
+    const errors = {};
+    if(!Validator.isEmail(data.email)){
+        errors.email = "Invalid email";
+    }
+    if(!data.password){
+        errors.password = "can't be blank";
+    }
+    return errors;
+  }
+
+  render() {
+    const { data, errors } = this.state;
+    return (
+      <Form onSubmit = {this.onSubmit}>
+        <Form.Field error = {!!errors.email}>
+          <Label htmlFor="email">Email</Label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="example@example.com"
+            value={data.email}
+            onChange={this.onChange}
+          />
+          {errors.email && <InlineError text = {errors.email}/>}
+        </Form.Field>
+        <Form.Field error = {!!errors.password}>
+          <Label htmlFor="password">Password</Label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Make it secue"
+            value={data.password}
+            onChange={this.onChange}
+          />
+          {errors.password && <InlineError text = {errors.password}/>}
+        </Form.Field>
+        <Button primary>Login</Button>
+      </Form>
+    );
+  }
+}
+
+export default LoginForm;
--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,3 +1,4 @@
+/* eslint-disabled */
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
